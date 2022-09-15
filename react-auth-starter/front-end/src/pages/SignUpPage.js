d0e48eaf0dbc8375e9753083c953719454477899
exphoenee <61277296+exphoenee@users.noreply.github.com>
2022-09-15 19:02:42 +0200

    Update SignUpPage.js

--- react-auth-starter/front-end/src/pages/SignUpPage.js
+++ react-auth-starter/front-end/src/pages/SignUpPage.js
@@ -1,7 +1,12 @@
 import { useState } from "react";
 import { useHistory } from "react-router-dom";
+import axios from "axios";
+
+/* Authentication */
+import { useToken } from "../auth/useToken";
 
 export const SignUpPage = () => {
+  const [token, setToken] = useToken();
   const [errorMessages, setErrorMessages] = useState(null);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
@@ -11,7 +16,16 @@ export const SignUpPage = () => {
   const onLoginClicked = () => {
     history.push("/login");
   };
-  const onRegisterClicked = () => {};
+
+  const onRegisterClicked = async () => {
+    const response = await axios.post("http://localhost:8080/api/signup", {
+      username,
+      password,
+    });
+    const { token } = response.data;
+    setToken(token);
+    history.push("/");
+  };
 
   return (
     <div className="content-container">
