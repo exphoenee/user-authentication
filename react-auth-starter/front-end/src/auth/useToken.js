6b0c2e1f4e1a02589a63507eff5b1259a8cd1924
exphoenee <61277296+exphoenee@users.noreply.github.com>
2022-09-15 19:02:36 +0200

    Create useToken.js

---
+++ react-auth-starter/front-end/src/auth/useToken.js
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+export const useToken = () => {
+  const [token, setInternalToken] = useState(() =>
+    localStorage.getItem("token")
+  );
+
+  const setToken = (newToken) => {
+    localStorage.setItem("token", newToken);
+    setInternalToken(newToken);
+  };
+
+  return [token, setToken];
+};
