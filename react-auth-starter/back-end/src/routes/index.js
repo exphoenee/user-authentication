783686084fda61fc04fc58aabcefe8db8ee2a8a1
exphoenee <61277296+exphoenee@users.noreply.github.com>
2022-09-16 11:30:44 +0200

    Update index.js

--- react-auth-starter/back-end/src/routes/index.js
+++ react-auth-starter/back-end/src/routes/index.js
@@ -1,5 +1,6 @@
 import { testRoute } from "./testRoute";
 import { signUpRoute } from "./signUpRoute";
 import { loginRoute } from "./loginRoute";
+import { updateUserInfoRoute } from "./updateUserInfoRoute";
 
-export const routes = [testRoute, signUpRoute, loginRoute];
+export const routes = [testRoute, signUpRoute, loginRoute, updateUserInfoRoute];
