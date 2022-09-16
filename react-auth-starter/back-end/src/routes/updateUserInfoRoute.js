1fc3b5e9fd0ca25f0c28574d5c53e53c20053754
exphoenee <61277296+exphoenee@users.noreply.github.com>
2022-09-16 12:01:02 +0200

    Update updateUserInfoRoute.js

--- react-auth-starter/back-end/src/routes/updateUserInfoRoute.js
+++ react-auth-starter/back-end/src/routes/updateUserInfoRoute.js
@@ -1,55 +1,56 @@
 import jwt from "jsonwebtoken";
 import { ObjectId } from "mongodb";
-
 import { getDbConnection } from "../db";
 
 export const updateUserInfoRoute = {
-  path: "/api/users/:id",
+  path: "/api/users/:userId",
   method: "put",
   handler: async (req, res) => {
     const { authorization } = req.headers;
     const { userId } = req.params;
 
-    const updates = ({ favoriteFood, hairColor, bio }) =>
-      ({ favoriteFood, hairColor, bio }(req.body));
+    const updates = (({ favoriteFood, hairColor, bio }) => ({
+      favoriteFood,
+      hairColor,
+      bio,
+    }))(req.body);
 
     if (!authorization) {
-      return res.status(401).json({ message: "No authorization header found" });
+      return res.status(401).json({ message: "No authorization header sent" });
     }
 
     const token = authorization.split(" ")[1];
 
     jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
-      if (err) {
-        return res.status(401).json({ message: "Unable to verify token!" });
-      }
+      if (err)
+        return res.status(401).json({ message: "Unable to verify token" });
 
       const { id } = decoded;
 
       if (id !== userId)
-        res
+        return res
           .status(403)
-          .json({ message: "Not allowed to update that user's data!" });
+          .json({ message: "Not allowed to update that user's data" });
 
       const db = getDbConnection("react-auth-db");
       const result = await db
         .collection("users")
-        .findOneAndUpadate(
+        .findOneAndUpdate(
           { _id: ObjectId(id) },
           { $set: { info: updates } },
           { returnOriginal: false }
         );
+      const { email, isVerified, info } = result.value;
 
-      const { email, isVerufied, info } = result.value;
       jwt.sign(
-        { id, email, isVerufied, info },
-        JWT_SECRET,
+        { id, email, isVerified, info },
+        process.env.JWT_SECRET,
         { expiresIn: "2d" },
         (err, token) => {
           if (err) {
-            res.status(200).json(err);
+            return res.status(200).json(err);
           }
-          res.json({ token });
+          res.status(200).json({ token });
         }
       );
     });
