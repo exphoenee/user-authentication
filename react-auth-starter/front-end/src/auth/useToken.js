f785cb8193540a69561c38f5826f6247ecfcb913
exphoenee <61277296+exphoenee@users.noreply.github.com>
2022-09-16 11:30:49 +0200

    Update useToken.js

--- react-auth-starter/front-end/src/auth/useToken.js
+++ react-auth-starter/front-end/src/auth/useToken.js
@@ -1,9 +1,9 @@
 import { useState } from "react";
 
 export const useToken = () => {
-  const [token, setInternalToken] = useState(() =>
-    localStorage.getItem("token")
-  );
+  const [token, setInternalToken] = useState(() => {
+    return localStorage.getItem("token");
+  });
 
   const setToken = (newToken) => {
     localStorage.setItem("token", newToken);
