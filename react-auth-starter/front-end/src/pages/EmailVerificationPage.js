28e00087cde528185af5f074b58d07150fb01913
exphoenee <61277296+exphoenee@users.noreply.github.com>
2022-09-16 20:57:30 +0200

    Update EmailVerificationPage.js

--- react-auth-starter/front-end/src/pages/EmailVerificationPage.js
+++ react-auth-starter/front-end/src/pages/EmailVerificationPage.js
@@ -1,27 +1,46 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+
 import { useToken } from "./../auth/useToken";
+import { EmailVerificationFailed } from "./EmailVerificationFailed";
+import { EmailVerificationSuccess } from "./EmailVerificationSuccess";
 
-export const VerifyEmailPage = () => {
-  const [message, setMessage] = useState("");
+export const EmailVerificationPage = () => {
+  //states isLoading and isSuccess defined here,
+  const [isLoading, setIsLoading] = useState(true);
+  const [isSuccess, setIsSuccess] = useState(false);
 
+  //added verification form url parameter
+  const verificationString = useParams();
+
+  //gets token form useToken
+  const [_, setToken] = useToken;
+
+  //in a useEffect hook, we make a request to the server to verify the email
   useEffect(() => {
-    const interval = setTimeout(() => {
-      const token = window.location.search.split("=")[1];
-      if (token) {
-        setMessage(
-          "Thank you for signing up! A verification email has been sent!"
+    const loadVerification = async () => {
+      try {
+        const response = await axios.post(
+          `http://localhost:8080/api/users/verify-email`,
+          {
+            verificationString,
+          }
         );
+        const { token } = response.data;
+        setToken(token);
+        setIsSuccess(true);
+        setIsLoading(false);
+      } catch (error) {
+        setIsSuccess(false);
+        setIsLoading(false);
       }
-    }, 3000);
-    return () => clearInterval(interval);
-  }, []);
+    };
+    loadVerification();
+  }, [setToken, verificationString]);
 
-  return (
-    <div>
-      <h1>{message}</h1>
-    </div>
-  );
+  if (isLoading) return <p>Loading...</p>;
+  if (!isSuccess) return <EmailVerificationFailed />;
+  return <EmailVerificationSuccess />;
 };
