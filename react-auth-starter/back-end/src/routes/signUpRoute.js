912c3b1b05017c4e068ae7c3c3271bbc1ebdf1c1
exphoenee <61277296+exphoenee@users.noreply.github.com>
2022-09-16 18:30:42 +0200

    Update signUpRoute.js

--- react-auth-starter/back-end/src/routes/signUpRoute.js
+++ react-auth-starter/back-end/src/routes/signUpRoute.js
@@ -1,6 +1,8 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import { getDbConnection } from "../db";
+import { v4 as uuid } from "uuid";
+import { sendEmail } from "../utils/sendEmail";
 
 export const signUpRoute = {
   path: "/api/signup",
@@ -20,21 +22,41 @@ export const signUpRoute = {
     const salt = await bcrypt.genSalt(saltRounds);
     const hashedPassword = await bcrypt.hash(password, salt);
 
+    // create a verification string
+    const verificationString = uuid();
+
+    // create a new user object
     const startingInfo = {
       hairColor: "",
       favoriteFood: "",
       bio: "",
     };
 
+    // insert the user into the database
     const result = await db.collection("users").insertOne({
       email,
       hashedPassword,
       info: startingInfo,
+      verificationString,
       isVerified: false,
     });
 
+    // get the id of the newly created user
     const { insertedId } = result;
 
+    try {
+      // send the verification email with the verification string as a link
+      await sendEmail({
+        to: email,
+        from: "viktor.bozzay@webforsol.hu",
+        subject: "Please verify your email",
+        text: `Please click on the following link to verify your email: http://localhost:3000/verify-email/${verificationString}`,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+
+    // sign a new token with the user's id and email
     jwt.sign(
       {
         id: insertedId,
