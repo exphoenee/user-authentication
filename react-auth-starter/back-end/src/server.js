21c5820b0051e02302f5c0e8e4cd922380fa51be
exphoenee <61277296+exphoenee@users.noreply.github.com>
2022-09-16 11:49:34 +0200

    Update server.js

--- react-auth-starter/back-end/src/server.js
+++ react-auth-starter/back-end/src/server.js
@@ -8,13 +8,13 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 
 // avoiding cors error the server runs on localhost port:8080 and the client runs on localhost port:3000
-const corsOptions = {
-  origin: "*",
-  methods: ["POST", "GET", "PATCH", "DELETE", "OPTIONS", "PUT"],
-  allowedHeaders: ["Content-Type", "Authorization", " application/json"],
-};
+// const corsOptions = {
+//   origin: "*",
+//   methods: ["POST", "GET", "PATCH", "DELETE", "OPTIONS", "PUT"],
+//   allowedHeaders: ["Content-Type", "Authorization", " application/json"],
+// };
 
-app.use(cors(corsOptions));
+//app.use(cors());
 
 // This allows us to access the body of POST/PUT
 // requests in our route handlers (as req.body)
