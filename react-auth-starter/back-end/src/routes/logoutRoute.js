0a4d77009486ccce8d0fc1c18e8a240014ec6752
exphoenee <61277296+exphoenee@users.noreply.github.com>
2022-09-21 20:47:36 +0200

    Update logoutRoute.js

--- react-auth-starter/back-end/src/routes/logoutRoute.js
+++ react-auth-starter/back-end/src/routes/logoutRoute.js
@@ -1,12 +1,9 @@
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
-import { getDbConnection } from "../db";
 import { log } from "../utils/logging";
 
 export const logoutRoute = {
   path: "/api/logout",
   method: "post",
-  handler: async (req, res) => {
-    log("logoutRoute: User logged out!");
+  handler: () => {
+    log("User logged out!");
   },
 };
