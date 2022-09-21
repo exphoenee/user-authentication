6e47cd407ef9f8b959d3488718a219b147bc81f1
exphoenee <61277296+exphoenee@users.noreply.github.com>
2022-09-21 20:06:03 +0200

    Create logging.js

---
+++ react-auth-starter/back-end/src/utils/logging.js
@@ -0,0 +1,13 @@
+export const log = (text) => {
+  console.log(
+    `${new Date().toLocaleString(undefined, {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+      hour: "2-digit",
+      hour12: false,
+      minute: "2-digit",
+      second: "2-digit",
+    })}: ${text}`
+  );
+};
