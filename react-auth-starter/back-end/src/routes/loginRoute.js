3795254eda87670baeb9ccc989480369092dcb79
exphoenee <61277296+exphoenee@users.noreply.github.com>
2022-09-21 20:05:44 +0200

    Update loginRoute.js

--- react-auth-starter/back-end/src/routes/loginRoute.js
+++ react-auth-starter/back-end/src/routes/loginRoute.js
@@ -1,16 +1,20 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import { getDbConnection } from "../db";
+import { log } from "../utils/logging";
 
 export const loginRoute = {
   path: "/api/login",
   method: "post",
   handler: async (req, res) => {
     const { email, password } = req.body;
-    const db = getDbConnection("react-auth-db");
-    const user = await db.collection("users").findOne({ email });
+    const db = getDbConnection(process.env.DBNAME);
+    const user = await db
+      .collection(process.env.USERSCOLLECTION)
+      .findOne({ email });
 
     if (user) {
+      log("User found in DB");
       const { _id: id, isVerified, hashedPassword, info } = user;
       //use bcript to compare the passwords
       const isPasswordCorrect = await bcrypt.compare(password, hashedPassword);
@@ -29,8 +33,10 @@ export const loginRoute = {
           (err, token) => {
             if (err) {
               res.status(500).json(err);
+              log("Error signing token");
             }
             res.status(200).send({ token });
+            log("JWT token created.");
           }
         );
       };
@@ -38,11 +44,14 @@ export const loginRoute = {
       //if the password is correct, sign a new token with the user's id and email
       if (isPasswordCorrect) {
         handleUser();
+        log("Password is correct!");
       } else {
         res.status(401).send("Incorrect password");
+        log("Password is invalid!");
       }
     } else {
       res.status(401).send("User not found");
+      log("User not found in DB!");
     }
   },
 };
