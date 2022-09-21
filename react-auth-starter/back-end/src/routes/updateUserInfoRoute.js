acf3a1f1ed27c0378eecdc55785c6febba0df525
exphoenee <61277296+exphoenee@users.noreply.github.com>
2022-09-21 20:05:55 +0200

    Update updateUserInfoRoute.js

--- react-auth-starter/back-end/src/routes/updateUserInfoRoute.js
+++ react-auth-starter/back-end/src/routes/updateUserInfoRoute.js
@@ -1,6 +1,7 @@
 import jwt, { verify } from "jsonwebtoken";
 import { ObjectId } from "mongodb";
 import { getDbConnection } from "../db";
+import { log } from "../utils/logging";
 
 export const updateUserInfoRoute = {
   path: "/api/users/:userId",
@@ -21,6 +22,7 @@ export const updateUserInfoRoute = {
 
     //if the authorization header is not present, send a 401 status code
     if (!authorization) {
+      log("No authorization header sent!");
       return res.status(401).json({ message: "No authorization header sent" });
     }
 
@@ -30,31 +32,35 @@ export const updateUserInfoRoute = {
     // verify the token
     jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
       //if there is an error, send a 401 status code and a message
-      if (err)
-        return res.status(401).json({ message: "Unable to verify token" });
+      if (err) {
+        log("Unable to verify token!");
+        return res.status(401).json({ message: "Unable to verify token!" });
+      }
 
       //decode the id and isVerified fields from the token
       const { id, isVerified } = decoded;
 
       //if the id from the token does not match the id from the url, send a 403 status code and a message
-      if (id !== userId)
+      if (id !== userId) {
+        log("Not allowed to update that user's data!");
         return res
           .status(403)
           .json({ message: "Not allowed to update that user's data" });
-
+      }
       //if the user is not verified, send a 403 status code and a message
       if (!isVerified) {
+        log("You need to verify your email before change to info.");
         return res.status(403).json({
           message: "You need to verify your email before change to info.",
         });
       }
 
       //getting the database connection
-      const db = getDbConnection("react-auth-db");
+      const db = getDbConnection(process.env.DBNAME);
 
       //updating the user's info in the db
       const result = await db
-        .collection("users")
+        .collection(process.env.USERSCOLLECTION)
         .findOneAndUpdate(
           { _id: ObjectId(id) },
           { $set: { info: updates } },
@@ -69,8 +75,10 @@ export const updateUserInfoRoute = {
         { expiresIn: "2d" },
         (err, token) => {
           if (err) {
+            log("JWT token sign failed.");
             return res.status(200).json(err);
           }
+          log("JWT token signed.");
           res.status(200).json({ token });
         }
       );
