17987d52cde2230e9e325e8aa3fce5e6b659a1cf
exphoenee <61277296+exphoenee@users.noreply.github.com>
2022-09-23 09:30:39 +0200

    Create PasswordResetFailed.js

---
+++ react-auth-starter/front-end/src/pages/PasswordResetFailed.js
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+
+export const PassWordResetFailed = () => {
+  return <></>;
+};
