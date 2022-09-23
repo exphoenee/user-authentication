11bb222491c50d1c7ace313e9a2ae06ea0d297f4
exphoenee <61277296+exphoenee@users.noreply.github.com>
2022-09-23 16:04:47 +0200

    Update PrivateRoutes.js

--- react-auth-starter/front-end/src/auth/PrivateRoutes.js
+++ react-auth-starter/front-end/src/auth/PrivateRoutes.js
@@ -1,10 +1,12 @@
 import { Route, Redirect } from "react-router-dom";
 import { useUser } from "./useUser";
 
-export const PrivateRoute = (props) => {
+const PrivateRoute = (props) => {
   const user = useUser();
 
   if (!user) return <Redirect to="/login" />;
 
   return <Route {...props} />;
 };
+
+export default PrivateRoute;
