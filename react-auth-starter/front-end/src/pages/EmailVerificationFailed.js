71f32d07bb31efe70fa843826c05655ef8a2416e
exphoenee <61277296+exphoenee@users.noreply.github.com>
2022-09-23 16:38:15 +0200

    Update EmailVerificationFailed.js

--- react-auth-starter/front-end/src/pages/EmailVerificationFailed.js
+++ react-auth-starter/front-end/src/pages/EmailVerificationFailed.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
 
+import { getRoute } from "../Routes";
+
 export const EmailVerificationFailed = () => {
   const history = useHistory();
 
   const handleRedirect = () => {
-    history.push("/");
+    history.push(getRoute("home"));
   };
 
   return (
