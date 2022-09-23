635a048f32255594f78f6e475f4716a5b760f61a
exphoenee <61277296+exphoenee@users.noreply.github.com>
2022-09-23 16:05:22 +0200

    Update Routes.js

--- react-auth-starter/front-end/src/Routes.js
+++ react-auth-starter/front-end/src/Routes.js
@@ -1,39 +1,74 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { UserInfoPage } from "./pages/UserInfoPage";
-import { LoginPage } from "./pages/LoginPage";
-import { SignUpPage } from "./pages/SignUpPage";
-import { PrivateRoute } from "./auth/PrivateRoutes";
-import { VerifyEmailPage } from "./pages/VerifyEmailPage";
-import { EmailVerificationPage } from "./pages/EmailVerificationPage";
-import { LogoutPage } from "./pages/LogoutPage";
-import { ForgotPasswordPage } from "./pages/ForgotPasswordPage";
+
+import PrivateRoute from "./auth/PrivateRoutes";
+
+import UserInfoPage from "./pages/UserInfoPage";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+import VerifyEmailPage from "./pages/VerifyEmailPage";
+import EmailVerificationPage from "./pages/EmailVerificationPage";
+import LogoutPage from "./pages/LogoutPage";
+import ForgotPasswordPage from "./pages/ForgotPasswordPage";
+import PasswordResetLandingPage from "./pages/PasswordResetLandingPage";
 
 export const Routes = () => {
   return (
     <Router>
       <Switch>
-        <PrivateRoute path="/" exact>
-          <UserInfoPage />
-        </PrivateRoute>
-        <Route path="/login">
-          <LoginPage />
-        </Route>
-        <Route path="/verify-email">
-          <VerifyEmailPage />
-        </Route>
-        <Route path="/email-verification/:verificationString">
-          <EmailVerificationPage />
-        </Route>
-        <Route path="/signup">
-          <SignUpPage />
-        </Route>
-        <Route path="/logout">
-          <LogoutPage />
-        </Route>
-        <Route path="/forgot-password">
-          <ForgotPasswordPage />
-        </Route>
+        {routing.map((route, index) => {
+          return route.privateRoute ? (
+            <PrivateRoute key={index} {...route}>
+              {route.component}
+            </PrivateRoute>
+          ) : (
+            <Route key={index} {...route}>
+              {route.component}
+            </Route>
+          );
+        })}
       </Switch>
     </Router>
   );
 };
+
+export const getRoute = (routeName) => {
+  const route = routing.find((route) => route.name === routeName);
+  return route.path;
+};
+
+export const routing = [
+  {
+    name: "home",
+    path: "/",
+    component: <UserInfoPage />,
+    exact: true,
+    privateRoute: true,
+  },
+  { name: "login", path: "/login", component: <LoginPage /> },
+  { name: "signup", path: "/signup", component: <SignUpPage /> },
+  {
+    name: "verifyemail",
+    path: "/verify-email",
+    component: <VerifyEmailPage />,
+  },
+  {
+    name: "emailverification",
+    path: "/email-verification/:verificationString",
+    component: <EmailVerificationPage />,
+  },
+  {
+    name: "logout",
+    path: "/logout",
+    component: <LogoutPage />,
+  },
+  {
+    name: "forgotpassword",
+    path: "/forgot-password",
+    component: <ForgotPasswordPage />,
+  },
+  {
+    name: "resetpassword",
+    path: "/reset-password/:passwordResetCode",
+    component: <PasswordResetLandingPage />,
+  },
+];
