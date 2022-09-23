6c9d1cc9ec1d1f3b0feb8281aa78e6cfb1beb2a0
exphoenee <61277296+exphoenee@users.noreply.github.com>
2022-09-23 16:38:29 +0200

    Update PasswordResetFailed.js

--- react-auth-starter/front-end/src/pages/PasswordResetFailed.js
+++ react-auth-starter/front-end/src/pages/PasswordResetFailed.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
 
+import { getRoute } from "../Routes";
+
 const PasswordResetFailed = () => {
   const history = useHistory();
 
   const handleRedirect = () => {
-    history.push("/");
+    history.push(getRoute("home"));
   };
 
   return (
