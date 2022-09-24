d0f5c155505a3513bcd893a1c517072c43e5ac28
exphoenee <61277296+exphoenee@users.noreply.github.com>
2022-09-24 12:59:30 +0200

    Create hashPassword.js

---
+++ react-auth-starter/back-end/src/services/hashPassword.js
@@ -0,0 +1,13 @@
+import bcrypt from "bcrypt";
+import { log } from "../utils/logging";
+
+const hashPassword = async (text) => {
+  try {
+    const salt = await bcrypt.genSalt(+process.env.SALT);
+    return await bcrypt.hash(text, salt);
+  } catch (error) {
+    log(error);
+  }
+};
+
+export default hashPassword;
