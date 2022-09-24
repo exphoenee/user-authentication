39e48c7698e4d5caf876a0013caa56db747070be
exphoenee <61277296+exphoenee@users.noreply.github.com>
2022-09-24 14:53:52 +0200

    Create sendEmail.js

---
+++ react-auth-starter/back-end/src/services/sendEmail.js
@@ -0,0 +1,17 @@
+import sendgrid from "@sendgrid/mail";
+import { log } from "./logging";
+
+sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
+
+export const sendEmail = ({ to, from, subject, text, html }) => {
+  const msg = { to, from, subject, text, html };
+
+  try {
+    sendgrid.send(msg).then(() => {
+      log(`Email sent to ${to} with following subject ${subject}`);
+    });
+  } catch (error) {
+    log(error);
+    res.status(500).send("error");
+  }
+};
