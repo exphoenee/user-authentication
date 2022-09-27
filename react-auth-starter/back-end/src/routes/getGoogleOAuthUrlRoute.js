1184264d6a9142820d38023feb61a732e7e6c0d2
exphoenee <bozzay.viktor@gmail.com>
2022-09-27 21:59:09 +0200

    Create getGoogleOAuthUrlRoute.js

---
+++ react-auth-starter/back-end/src/routes/getGoogleOAuthUrlRoute.js
@@ -0,0 +1,10 @@
+import getGoogleOAuthUrl from "../util/getGoogleOauthUrl";
+
+export const getGoogleOAuthUrlRouth = {
+  path: "/auth/google/url",
+  method: "get",
+  handler: async (req, res) => {
+    const url = getGoogleOAuthUrl();
+    res.status(200).send(url);
+  },
+};
