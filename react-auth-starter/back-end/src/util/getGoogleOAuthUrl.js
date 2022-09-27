1b421f97c96bb9973d5559bed5183e5e32f0fccb
exphoenee <bozzay.viktor@gmail.com>
2022-09-27 21:59:15 +0200

    Create getGoogleOAuthUrl.js

---
+++ react-auth-starter/back-end/src/util/getGoogleOAuthUrl.js
@@ -0,0 +1,18 @@
+import oAuthClient from "./oAuthClient";
+
+const getGoogleOAuthUrl = () => {
+  const scopes = [
+    "https://www.googleapis.com/auth/userinfo.email",
+    "https://www.googleapis.com/auth/userinfo.profile",
+  ];
+
+  const url = oAuthClient.generateAuthUrl({
+    access_type: "offline",
+    prompt: "consent",
+    scope: scopes,
+  });
+
+  return url;
+};
+
+export default getGoogleOAuthUrl;
