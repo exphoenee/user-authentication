351eff0da64e579c569befd7dcf8fc465b205d30
exphoenee <bozzay.viktor@gmail.com>
2022-09-28 20:24:55 +0200

    Update verifyEmailRoute.js

--- react-auth-starter/back-end/src/routes/verifyEmailRoute.js
+++ react-auth-starter/back-end/src/routes/verifyEmailRoute.js
@@ -2,37 +2,37 @@ import { ObjectId } from "mongodb";
 import { getDbConnection } from "../db";
 import { log } from "../services/logging";
 
+import { CogintoUser } from "amazon-cognito-identity-js";
+import { awsUserPool } from "../services/awsUserPool";
+
 export const verifyEmailRoute = {
   path: "/api/verifyemail",
   method: "put",
   handler: async (req, res) => {
     //first getting the everification string form the url
-    const { verificationString } = req.body;
+    const { email, verificationString } = req.body;
 
-    //getting the database connection
-    const db = getDbConnection(process.env.DBNAME);
-
-    //finding the user with the verification string
-    const user = await db
-      .collection(process.env.USERSCOLLECTION)
-      .findOne({ verificationString });
-
-    //if the user is not found, send a 404 status code
-    if (!user) {
-      log("The email email verification code is incorrect!!");
-      return res
-        .status(401)
-        .send({ message: "The email email verification code is incorrect!" });
-    }
-
-    const { _id: id, email, info } = user;
-
-    //if the user is found, update the user's isVerified field to true
-    await db
-      .collection(process.env.USERSCOLLECTION)
-      .updateOne({ _id: ObjectId(id) }, { $set: { isVerified: true } });
-
-    //signing a new token with the updated user data
-    createToken(id, email, true, info);
+    new CognitoUser({
+      Username: email,
+      Pool: awsUserPool,
+    }).confirmRegistration(verificationString, true, async (err, result) => {
+      if (err) {
+        log(err);
+        res.status(401).send({ message: "Verification failed" });
+      } else {
+        log("Verification successful");
+        res.status(200).send("Verification successful");
+        const db = getDbConnection(process.env.DBNAME);
+        const user = await db
+          .collection(process.env.USERSCOLLECTION)
+          .findOneAndUpdate(
+            { verificationString },
+            { $set: { isVerified: true } },
+            { returnOriginal: false }
+          );
+        const { _id: id, info } = user.value;
+        createToken(id, email, true, info);
+      }
+    });
   },
 };
