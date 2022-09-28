d9db6baf2f791cc2e9d7b29db3857144249cf367
exphoenee <bozzay.viktor@gmail.com>
2022-09-28 20:24:47 +0200

    Update forgotPasswordRoute.js

--- react-auth-starter/back-end/src/routes/forgotPasswordRoute.js
+++ react-auth-starter/back-end/src/routes/forgotPasswordRoute.js
@@ -1,8 +1,6 @@
-import { v4 as uuid } from "uuid";
+import { CognitoUser } from "amazon-cognito-identity-js";
 
-import sendEmail from "../services/sendEmail";
-import { getDbConnection } from "../db";
-import { log } from "../services/logging";
+import awsUserPool from "../util/awsUserPool";
 
 export const forgotPasswordRoute = {
   path: "/api/forgot-password/:email",
@@ -10,21 +8,17 @@ export const forgotPasswordRoute = {
   handler: async (req, res) => {
     const { email } = req.params;
 
-    const passwordResetCode = uuid();
-
-    const db = getDbConnection(process.env.DBNAME);
-
-    const user = await db
-      .collection(process.env.USERSCOLLECTION)
-      .updateOne({ email }, { $set: { passwordResetCode } });
-
-    if (user.modifiedCount > 0) {
-      sendEmail({
-        to: email,
-        from: process.env.SENDERMAIL,
-        subject: "Password Reset",
-        text: `To reset your password click this link: http://localhost:3000/reset-password/${passwordResetCode}`,
-      });
-    }
+    new CognitoUser({
+      Username: email,
+      Pool: awsUserPool,
+    }).forgotPassword({
+      onSuccess: (result) => {
+        res.status(200).send(result);
+      },
+      onFailure: (err) => {
+        log(err.message);
+        res.status(401).send(err.message);
+      },
+    });
   },
 };
