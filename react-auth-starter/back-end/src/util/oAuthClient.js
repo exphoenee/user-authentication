da76d90a0af3f86119f183f4016a6bbc4b920259
exphoenee <bozzay.viktor@gmail.com>
2022-09-28 17:45:14 +0200

    Update oAuthClient.js

--- react-auth-starter/back-end/src/util/oAuthClient.js
+++ react-auth-starter/back-end/src/util/oAuthClient.js
@@ -1,9 +1,7 @@
-import {google} from googleapis;
+import {google} from "googleapis";
 
-const oAuthClient = new google.auth.OAuth2(
+export const oAuthClient = new google.auth.OAuth2(
   process.env.GOOGLE_CLIENT_ID,
   process.env.GOOGLE_CLIENT_SECRET,
   process.env.GOOGLE_REDIRECT_URI
 )
-
-export default oAuthClient;
