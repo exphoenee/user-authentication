3fbc3904ba229b51483d3361f783568118610d3d
exphoenee <bozzay.viktor@gmail.com>
2022-09-28 20:33:11 +0200

    Update PasswordResetLandingPage.js

--- react-auth-starter/front-end/src/pages/PasswordResetLandingPage.js
+++ react-auth-starter/front-end/src/pages/PasswordResetLandingPage.js
@@ -4,20 +4,23 @@ import axios from "axios";
 
 import PassWordResetFailed from "./PasswordResetFailed";
 import PasswordResetSuccess from "./PasswordResetSuccess";
+import { useQueryParams } from "./../util/useQueryParams";
 
 const PasswordResetLandingPage = () => {
   const [passwordValue, setPasswordValue] = useState("");
   const [confirmPasswordValue, setConfirmPasswordValue] = useState("");
+  const [resetCode, setResetCode] = useState("");
   const [isSuccess, setIsSuccess] = useState(false);
   const [isFailure, setIsFailure] = useState(false);
 
-  const { passwordResetCode } = useParams();
+  const { email } = useQueryParams();
 
   const onPasswordResetClicked = async () => {
     try {
       await axios.put(
-        `http://localhost:8080/api/users/${passwordResetCode}/reset-password`,
+        `http://localhost:8080/api/users/${resetCode}/reset-password`,
         {
+          email,
           newPassword: passwordValue,
         }
       );
@@ -45,6 +48,12 @@ const PasswordResetLandingPage = () => {
         value={confirmPasswordValue}
         onChange={(e) => setConfirmPasswordValue(e.target.value)}
         placeholder="Confirm password"
+      />
+      <input
+        type="text"
+        value={resetCode}
+        onChange={(e) => setResetCode(e.target.value)}
+        placeholder="reset code"
       />
       <button
         disabled={
